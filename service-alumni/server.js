870cffda7a90ffacae4a2e875c1ee0afcb39a327
Hugo Gallazzini <1359916+stornitz@users.noreply.github.com>
2019-12-11 09:48:39 +0100

    alumni - add connection to mongodb

--- service-alumni/server.js
+++ service-alumni/server.js
@@ -1,16 +1,24 @@
 'use strict';
-
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
 // Constants
+const MONGODB_URI = 'mongodb://database-alumni:27017';
 const PORT = 8080;
-const HOST = '0.0.0.0';
 
 // App
 const app = express();
+var mongodb;
+
+MongoClient.connect(MONGODB_URI, function(err, database) {
+  if(err) throw err;
+ 
+  mongodb = database;
+
+  app.listen(PORT);
+  console.log(`Listening on port ${PORT}`);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
