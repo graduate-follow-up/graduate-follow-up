d2a1de54ee900555cecbdafd9dbddff4f3f58758
Alexandre <alexandre.tournier@eisti.fr>
2020-05-16 13:54:22 +0200

    creation fct mail

---
+++ service_mail/app/services/mail.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import org.intracer.wmua.User
+import play.api.libs.mailer.{Email, MailerPlugin}
+
+trait SendMail {
+  def sendMail(from: User, to: User, subject: String, message: String)
+}
+
+class SendMailSMTP extends SendMail {
+
+  override def sendMail(fromUser: User, toUser: User, subject: String, message: String) = {
+    val email = Email(
+      from = s"${fromUser.fullname} <${fromUser.email}>",
+      to = Seq(toUser.email),
+      subject = subject,
+      bodyText = Some(message)
+    )
+    MailerPlugin.send(email)(play.api.Play.current)
+  }
+} 
