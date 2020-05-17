3c1b6dc29bc0eaa653bb3633b76f94fbbd4f7912
AlexDissaux <dissauxalex@gmail.com>
2020-05-17 13:54:21 +0200

    work on request post handling error.. not finish yet

--- front/src/app/alumnus/service/alumnus.service.ts
+++ front/src/app/alumnus/service/alumnus.service.ts
@@ -3,65 +3,28 @@ import {Alumnus} from '../../model/Alumnus';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
-import {AlumnusWithoutId} from "../../model/alumnus-without-id";
+import {AlumnusWithoutId} from '../../model/alumnus-without-id';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlumnusService {
 
+  constructor(private http: HttpClient) {}
+
   private ulrAlumnus = 'http://localhost/alumnis/';
   private alumnusData: Alumnus[];
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-
-  constructor(private http: HttpClient) {}
-
-  getAlumnusObservable(): Observable<Alumnus[]> {
-    return this.http.get<Alumnus[]>(this.ulrAlumnus)
-      .pipe(catchError(this.handleError)
-      );
-  }
-
-  getAlumnusFillData(): void {
-    this.getAlumnusObservable().subscribe(alumnus => {
-      console.log(alumnus);
-      this.alumnusData = alumnus;
-    });
-    console.log(this.alumnusData);
-  }
-
-  getAlumnus(): Alumnus[] {
-    return this.alumnusData; }
-
- /* getAlumnusIndex(id: number): number {
-    return this.getAlumnus().findIndex(e => e._id === id); }
-
-  generateId(): number {
-    return this.getAlumnus().reduce(((acc, val) => (val._id > acc) ? val._id : acc), 0) + 1; }
-*/
-  add(newAlumnus: Alumnus) { // Insert in Database
-    this.getAlumnus().push(newAlumnus); }
-
-  /*delete(id: number) {
-    const index: number = this.getAlumnus().findIndex(e => e._id === id);
-    this.getAlumnus().splice(index, 1);
-  }*/
-
-  update(id: string, alumnus: AlumnusWithoutId) {
-    return this.http.put(this.ulrAlumnus + '/' + id, alumnus, this.httpOptions)
-      .pipe(catchError(this.handleError)
-    );
-  }
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})};
 
  /* findOne(alumnusId: number): Alumnus {
     return this.getAlumnus().find(e => e._id === alumnusId);
   }*/
 
 
-  private handleError(error: HttpErrorResponse) {
+  private static handleError(error: HttpErrorResponse) {
+    console.log('genre ya pas d\'erreur la ?' + error);
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
@@ -75,6 +38,52 @@ export class AlumnusService {
     // return an observable with a user-facing error message
     return throwError(
       'Something bad happened; please try again later.');
+  }
+
+  getAlumnusObservable(): Observable<Alumnus[]> {
+    return this.http.get<Alumnus[]>(this.ulrAlumnus)
+      .pipe(catchError(AlumnusService.handleError)
+      );
+  }
+
+  /*getAlumnusFillData(): void {
+    this.getAlumnusObservable().subscribe(alumnus => {
+      console.log(alumnus);
+      this.alumnusData = alumnus;
+    });
+    console.log(this.alumnusData);
+  }*/
+
+  getAlumnus(): Alumnus[] {
+    return this.alumnusData; }
+
+ /* getAlumnusIndex(id: number): number {
+    return this.getAlumnus().findIndex(e => e._id === id); }
+
+  generateId(): number {
+    return this.getAlumnus().reduce(((acc, val) => (val._id > acc) ? val._id : acc), 0) + 1; }
+*/
+  add(newAlumnus: AlumnusWithoutId) { // Insert in Database
+    this.http.post<string>(this.ulrAlumnus, newAlumnus, this.httpOptions)
+      .pipe(catchError(AlumnusService.handleError))
+     /* .subscribe({
+      next: data => console.log('data id = ' + data),
+      error: error => console.error('There was an error!', error)
+    })*/
+     /* .pipe(catchError(this.handleError)
+      )*/;
+    console.log(newAlumnus);
+
+  }
+
+  /*delete(id: number) {
+    const index: number = this.getAlumnus().findIndex(e => e._id === id);
+    this.getAlumnus().splice(index, 1);
+  }*/
+
+  update(id: string, alumnus: AlumnusWithoutId) {
+    return this.http.put(this.ulrAlumnus + '/' + id, alumnus, this.httpOptions)
+      .pipe(catchError(AlumnusService.handleError));
   }
 
 }
