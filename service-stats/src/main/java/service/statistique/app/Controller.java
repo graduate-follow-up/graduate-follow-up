458431906340953a9684ed6983c6fa21504fe4dc
AlexDissaux <dissauxalex@gmail.com>
2020-05-20 15:05:52 +0200

    minor update

--- service-stats/src/main/java/service/statistique/app/Controller.java
+++ service-stats/src/main/java/service/statistique/app/Controller.java
@@ -3,11 +3,34 @@ package service.statistique.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @RestController
 public class Controller {
 
-    @GetMapping("/test")
-    String test () {
-        return "test succeeded";
+    @GetMapping("/")
+    String test () throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost/alumnis"))
+                .build();
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+        /*HttpURLConnection con = (HttpURLConnection) new URL("http://localhost/alumnis").openConnection();
+
+        con.setRequestMethod("GET");*/
+
+        return response.body();
     }
 }
