26f861b6b96112fbe4c9058d3430863c3860eac4
Hugo Gallazzini <1359916+stornitz@users.noreply.github.com>
2020-05-26 01:15:59 +0200

    [service-connexion] use axios instead of http

--- service-connexion/server.js
+++ service-connexion/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const http = require('http');
+const axios = require('axios');
 
 if(! process.env.JWT_ACCESS_TOKEN_SECRET || ! process.env.JWT_REFRESH_TOKEN_SECRET){
     console.error('\x1b[31m%s\x1b[0m', 'Jwt tokens are not initialized. Please run install.sh.');
@@ -26,72 +26,46 @@ app.listen(PORT, () => {
     // refresh-token : token permettant de régénérer accesstoken
 
 app.post('/login', (req, res) => {
-    const username = req.body.user;
-    const pwd = req.body.password;
-    console.log(username);
-    console.log(pwd);
+    const {user: username, password: pwd} = req.body;
 
-    const body_to_send = JSON.stringify({user : username, password: pwd})
+    axios.post('http://service_user:3000/check-user', {
+        user : username,
+        password: pwd
+    }).then(result => {
+        const {username, statut: role, _id: id} = result.data;
+        
+        const accessToken = jwt.sign(
+            {
+                username: username,
+                role: role,
+                id: id
+            },
+            process.env.JWT_ACCESS_TOKEN_SECRET,
+            {expiresIn: '20m'}
+        );
 
-    const options = {
-        host: 'service_user',
-        port: 3000,
-        path: '/check-user',
-        method: 'GET',
-        family: 4,
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': body_to_send.length
+        const refreshToken = jwt.sign(
+            {
+                username: username,
+                role: role,
+                id: id },
+            process.env.JWT_REFRESH_TOKEN_SECRET,
+            {expiresIn: '120m'}
+        );
+
+        refreshTokens.push(refreshToken);
+
+        res.json({
+            accessToken,
+            refreshToken
+        });
+    }).catch(error => {
+        if(error.response && error.response.status == 404) {
+            res.status(401).send('Username or password incorrect');
+        } else {
+            res.sendStatus(500);
         }
-    }
-
-    const request = http.request(options, result => {
-        var responseString = "";
-
-        result.on("data", function (data) {
-            responseString += data;
-        });
-        result.on("end", function () {
-            if (result.statusCode === 200){
-                responseString = JSON.parse(responseString);
-                const accessToken = jwt.sign(
-                    {
-                        username: username,
-                        role: responseString.statut,
-                        id: responseString._id
-                    },
-                    process.env.JWT_ACCESS_TOKEN_SECRET,
-                    {expiresIn: '20m'}
-                );
-
-                const refreshToken = jwt.sign(
-                    {
-                        username: username,
-                        role: responseString.statut,
-                        id: responseString._id },
-                    process.env.JWT_REFRESH_TOKEN_SECRET,
-                    {expiresIn: '120m'}
-                );
-
-                refreshTokens.push(refreshToken);
-
-                res.json({
-                    accessToken,
-                    refreshToken
-                });
-            }else{
-                res.status(404).send('Username or password incorrect');
-            }
-        });
     });
-
-
-    request.on('error', error => {
-        console.error(error);
-    });
-    request.write(body_to_send);
-    request.end();
-
 });
 
 // A SUPPRIMER QUAND DEV FINI
