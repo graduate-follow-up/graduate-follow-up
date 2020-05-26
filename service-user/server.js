725dd376374ad7b6367a10e2670369a9460711c4
Chloé Trouilh <chloe.trouilh@gmail.com>
2020-05-26 12:37:35 +0200

    [front-connexion] Add password security

--- service-user/server.js
+++ service-user/server.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient, ObjectId } = require('mongodb');
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 // Constants
 const MONGODB_URI = 'mongodb://database_user:27017/users';
 const DATABASE_NAME = 'users';
 const COLLECTION_NAME = 'users';
+const SALT_ROUND = 17 ;
 
 const PORT = 3000;
 
@@ -46,14 +49,25 @@ app.get('/check-user',(req, res) => {
   const usr = req.body.user;
   const pwd = req.body.password;
 
-  collection.find({login: usr, mdp: pwd }).project({ statut: 1 }).toArray(function (err, docs) {
+  collection.find({login: usr}).project({ statut: 1, salt : 1, hashed: 1 }).toArray(function (err, docs) {
     if(err) {
       res.status(500).send(err);
     } else {
+      // If login is not found
       if(docs.length === 0){
         res.status(404).send();
       }else{
-        res.status(200).send(docs[0]);
+        bcrypt.compare( pwd +docs[0].salt , docs[0].hashed, function(err, response) {
+          if(response) {
+            delete docs[0]['salt'];
+            delete docs[0]['hashed'];
+            res.status(200).send(docs[0]);
+          } else {
+            // hash don't match
+            res.status(404).send();
+          }
+        });
+
       }
     }
   });
@@ -62,7 +76,7 @@ app.get('/check-user',(req, res) => {
 });
 
 app.get('/:userId', (req, res) => {
-  collection.find({_id: ObjectId(req.params.userId)}).toArray(function (err, docs) {
+  collection.find({_id: ObjectId(req.params.userId)}).project({salt:0, hashed: 0}).toArray(function (err, docs) {
     if(err) {
       res.status(500).send(err);
     } else {
@@ -80,14 +94,30 @@ app.post('/', (req, res) => {
   // TODO check permissions
   // TODO verify document format
   let document = req.body;
+  let password = req.body.password;
+  delete document['password'];
+  let salt ;
+  console.log('starting');
 
-  collection.insertOne(document, (err, resMongo) => {
-    if(err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(resMongo.insertedId);
-    }
-  });
+  crypto.randomBytes(256, (err, buf) => {
+    if (err) res.status(500).send(err);
+
+    salt = buf.toString('hex');
+    document["salt"] = salt;
+
+    bcrypt.hash(password + salt, SALT_ROUND, function(err, hash) {
+      if (err) res.status(500).send(err);
+
+      document["hashed"] = hash;
+      collection.insertOne(document, (err, resMongo) => {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          res.status(200).send(resMongo.insertedId);
+        }
+      });
+    });
+  })
 })
 
 
@@ -95,7 +125,15 @@ app.put('/:userId', (req, res) => {
   // TODO check permissions
   // TODO verify update content
 
-  let update = {$set : req.body};
+  let update = req.body;
+
+  // if (req.body.salt) delete update['salt'] ;
+  // if (req.body.hashed) delete update['hashed'];
+  if (req.body.password) delete update['password'] ;
+  if (req.body.statut) delete update['statut'] ;
+  // if (req.body.role) delete update['role];
+
+  update = {$set : update};
 
   collection.updateOne({_id: ObjectId(req.params.userId)}, update, (err,resMongo) => {
     if(err) {
